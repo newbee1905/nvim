564cfc394824712e4001ffa28056e9e10704d201
newbee1905 <beenewminh@outlook.com>
2022-08-22 07:45:48 +0700

    feat: Add few functions for ultilities for nightfox

--- lua/utils/init.lua
+++ lua/utils/init.lua
@@ -1,11 +1,15 @@
-utils = {}
+local has = vim.fn.has
 
-function utils:shallow_copy(orig)
+local M = {}
+
+---Shallow copy table -> Only copy value
+---@param org table
+function M.shallow_copy(org)
 	local copy
-	if type(orig) == 'table' then
+	if type(org) == 'table' then
 		copy = {}
-		for i = 1, #orig do
-			copy[i] = orig[i]
+		for i = 1, #org do
+			copy[i] = org[i]
 		end
 	else -- number, string, etc.
 		copy = org
@@ -13,4 +17,21 @@ function utils:shallow_copy(orig)
 	return copy
 end
 
-return utils
+---Clamp value between the min and max values.
+---@param value number
+---@param min number
+---@param max number
+function M.clamp(value, min, max)
+  if value < min then
+    return min
+  elseif value > max then
+    return max
+  end
+  return value
+end
+
+
+M.is_nvim = has("nvim") == 1
+M.use_nvim_api = M.is_nvim and has("nvim-0.7") == 1
+
+return M
