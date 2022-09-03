505e13816ec435d256d6786bf2a601e3b2bdf7a5
newbee1905 <beenewminh@outlook.com>
2022-09-04 01:19:26 +0700

    feat: move to cmp

--- lua/plugins/modules/completion/plugins.lua
+++ lua/plugins/modules/completion/plugins.lua
@@ -15,3 +15,15 @@ use 'ms-jpq/coq.thirdparty' {
 	after = 'coq_nvim',
 	branch = '3p',
 }
+
+use "hrsh7th/nvim-cmp" {
+	event = "InsertEnter",
+	requires = {
+		{ "hrsh7th/cmp-nvim-lsp", after = "nvim-cmp" },
+		{ "hrsh7th/cmp-buffer", after = "nvim-cmp" },
+		{ "hrsh7th/cmp-path", after = "nvim-cmp" },
+		{ "ray-x/cmp-treesitter", after = "nvim-cmp" }
+		-- { "saadparwaiz1/cmp_luasnip", after = "LuaSnip" },
+	},
+	config = conf.cmp,
+}
